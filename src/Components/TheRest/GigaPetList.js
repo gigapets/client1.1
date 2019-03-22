1b2dc09597ad020ce9980bcbac3fb545d1e894da
Eli Citron <ebcitron@gmail.com>
2019-03-22 15:00:07 -0500

    Fixing CRUD stuff

--- src/Components/TheRest/GigaPetList.js
+++ src/Components/TheRest/GigaPetList.js
@@ -7,104 +7,106 @@ import DayPosts from "./DayPosts";
 import axios from "axios";
 import { NavLink, Route } from "react-router-dom";
 
-
 class GigaPetList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dayPosts: []
+    constructor(props) {
+      super(props);
+      this.state = {
+        dayPosts: [],
+        error: ""
+      };
+    }
+
+    componentDidMount() {
+      console.log("inside Component Did Mount", this.state);
+      axios
+        .get("https://gigapets.herokuapp.com/gigapets")
+        .then(response => this.setState({ dayPosts: response.data }))
+        .catch(err => console.log("ComponentDidMount error: ", err));
+    }
+
+    addDayPost = (event, dayPost) => {
+      axios
+        .post("https://gigapets.herokuapp.com/gigapets", dayPost)
+        .then(response => this.setState({ dayPosts: response.data }))
+        .catch(err => console.log("Add Day Post Error: ", err));
     };
-  }
 
-  componentDidMount() {
-    console.log("inside Component Did Mount", this.state);
-    axios
-      .get("https://gigapets.herokuapp.com/gigapets")
-      .then(response => this.setState({ dayPosts: response.data }))
-      .catch(err => console.log("ComponentDidMount error: ", err));
-  }
+    // deleteDayPost = (event, id) => {
+    //   event.preventDefault();
+    //   axios
+    //
 
-  addDayPost = (event, dayPost) => {
-    axios
-      .post("https://gigapets.herokuapp.com/gigapets", dayPost)
-      .then(response => this.setState({ dayPosts: response.data }))
-      .catch(err => console.log("Add Day Post Error: ", err));
-  };
+    // };
 
-  // deleteDayPost = (event, id) => {
-  //   event.preventDefault();
-  //   axios
-  //
+  deletePost = () => {
+      event.preventDefault();
+      axios
+        .delete(`https://gigapets.herokuapp.com/gigapets/${id}`)
+        .then(response => console.log('DeleteResponse:', response))
+        .catch(err => console.log("DeleteError:", err)
+    }
+  }  
+        
 
-  // };
-
-  deleteDayPost = (event, id) => {
-    event.preventDefault();
-
-    axios
-      .delete(`https://gigapets.herokuapp.com/gigapets/${id}`)
-      .then(response => {
-        this.setState({ dayPosts: response.data }).catch(err =>
-          console.log("error:", err)
-        );
-      });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <div className="navBar" id = "nav">
-          
+    render() {
+      return (
+        <div className="App">
+          <div className="navBar" id="nav">
             <NavLink exact to="/">
-            <div id = "navLink">Home</div>
+              <div id="navLink">Home</div>
             </NavLink>
-          
+
             <NavLink to="/DayForm">
-              <div id = 'navLink' >Record Something</div>
-              </NavLink>
+              <div id="navLink">Record Something</div>
+            </NavLink>
           </div>
-         
 
-      
-            <Route
-          exact
-          path="/"
-          render={props => (
-            <DayPosts
-              {...props}
-              dayPosts={this.state.dayPosts}
-              handleChanges={this.handleChanges}
-              deleteDayPost={this.deleteDayPost}
-              updateDayPost={this.updateDayPost}
-            />
-          )}
-        />
-        <Route
-          exact
-          path="/DayForm"
-          render={props => <DayForm {...props} addDayPost={this.addDayPost} />}
-        />
+          <Route
+            exact
+            path="/"
+            render={props => (
+              <DayPosts
+                {...props}
+                dayPosts={this.state.dayPosts}
+                handleChanges={this.handleChanges}
+                deletePost={this.deletePost}
+                updateDayPost={this.updateDayPost}
+              />
+            )}
+          />
 
-       
-        {/* <Route
-        exact path = "/week"
-        render = {props => <DayPosts {...props} dayPosts = {this.state.dayPosts} />}
-        /> */}
-        <Route
-          path="/gigapets/:id"
-          render={props => (
-            <DayPost
-              {...props}
-              dayPosts={this.state.dayPosts}
-              handleChanges={this.handleChanges}
-              deleteDayPost={this.deleteDayPost}
-              updateDayPost={this.updateDayPost}
+          <Route 
+            exact path="/deletePost"
+            render ={props => (deletePost={this.deletePost}
+            deleteSuccess={this.deleteSuccess}
+            deleteError={this.deleteError}
             />
-          )}
-        />
-      </div>
-    );
-  }
+            )}
+            />
+
+          <Route
+            exact
+            path="/DayForm"
+            render={props => <DayForm {...props} addDayPost={this.addDayPost} />}
+          />
+
+
+          <Route
+            exact path= '/'
+            path="/gigapets/:id"
+            render={props => (
+              <DayPost
+                {...props}
+                dayPosts={this.state.dayPosts}
+                handleChanges={this.handleChanges}
+                deleteDayPost={this.deleteDayPost}
+                updateDayPost={this.updateDayPost}
+              />
+            )}
+          />
+        </div>
+      );
+    }
 }
 
 export default GigaPetList;
